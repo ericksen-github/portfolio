97a9210d0cca2f7e71eff52e0a4ed6fa663c9332
James <ericksen.github@gmail.com>
2020-08-10 12:47:34 +0100

    Updated header. Added navbar.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { stickNavBar } from "./modules/navbar";
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  stickNavBar();
+};
---
+++ src/modules/navbar.js
@@ -0,0 +1,17 @@
+// Get the navbar
+const navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position.
+// Remove "sticky" when you leave the scroll position.
+const stickNavBar = () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+export { stickNavBar };
